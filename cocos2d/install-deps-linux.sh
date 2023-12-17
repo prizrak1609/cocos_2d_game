84eb4855de6889ed955a7a6eb47efb31dc595c7a
Dima <gubatenkod@icloud.com>
2023-12-17 19:30:51 +0200

    Fixed linux install deps script

--- cocos2d/install-deps-linux.sh
+++ cocos2d/install-deps-linux.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 echo "This Shell Script will install dependencies for cocos2d-x" 
-echo -n "Are you continue? (y/n) "
-read answer
-if echo "$answer" | grep -iq "^y" ;then
-    echo "It will take few minutes"
-else
-    exit
-fi
 
-sudo apt-get update
+sudo apt update
 
 # run 32bit applicatio: needed for lua relase mode as luajit has 32bit version
 # https://askubuntu.com/questions/454253/how-to-run-32-bit-app-in-ubuntu-64-bit
@@ -32,4 +25,4 @@ DEPENDS+=' libssl-dev'
 DEPENDS+=' libgtk-3-dev'
 DEPENDS+=' binutils'
 
-sudo apt-get install --force-yes --yes $DEPENDS > /dev/null
+sudo apt install --force-yes --yes $DEPENDS > /dev/null
